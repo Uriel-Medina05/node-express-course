b91b5f859b1e04a8a62ea7efd1bb729da1ffe123
Uriel Medina <umedina@unitedvirtualities.com>
2020-02-05 16:01:15 -0700

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
  const express = require ('express');
  const app = express();
 
+const  mockUserData = [
+{nombre : ' Mark ' },
+{nombre : ' Jill ' }
+]
+aplicación . get ( ' / users ' , function ( req , res ) {
+ 	 res . json ({
+ 	 	el éxito :  verdadero ,
+ 	 	mensaje :  ' consiguió usuarios con éxito. ¡Agradable! ' ,
+ 	 	usuarios : mockUserData
+ 	})
+})
+
  app.listen(8000,function(){
 console.log("server is running")
 })
